ac93e0af61e1c32d52646f52e31361e00e45a7f0
vera98x <v.schoonderwoerd@outlook.com>
2023-01-17 16:31:32 +0100

    added interventionrate

--- Tests/ETL_dataTests.py
+++ Tests/ETL_dataTests.py
@@ -6,77 +6,98 @@ import numpy as np
 from TrainRideNode import TrainRideNode
 
 from ETL_data import keepWeekendDays, keepWorkDays
-def createTestSample():
-    d = {'basic|uitvoer': ["1-3-2019 05:26:01", "1-3-2019 05:27:00", "1-3-2019 05:36:00","1-3-2019  05:37:00"],
-         'delay': [0,1,2,1],
-         'basic|drp_act': ["V", "D", "A","D"],
-         'basic_treinnr_treinserie': ["500E","500E","500E","500E"],
-         'basic|treinnr': ["501","501","501","501"]}
+# def createTestSample():
+#     d = {'basic|uitvoer': ["1-3-2019 05:26:01", "1-3-2019 05:27:00", "1-3-2019 05:36:00","1-3-2019  05:37:00"],
+#          'delay': [0,1,2,1],
+#          'basic|drp_act': ["V", "D", "A","D"],
+#          'basic_treinnr_treinserie': ["500E","500E","500E","500E"],
+#          'basic|treinnr': ["501","501","501","501"]}
+#     df = pd.DataFrame(data=d)
+#
+#     df['basic|uitvoer'] = pd.to_datetime(df['basic|uitvoer'], format='%m-%d-%Y %H:%M:%S')
+#     df['basic|plan'] = df['basic|uitvoer']
+#     df['date'] = pd.to_datetime(df['basic|uitvoer']).dt.date
+#     return df
+#
+# def keepActivity(df_input, act_val : List[str]):
+#     df_input = df_input.loc[df_input['basic|drp_act'].isin(act_val)]
+#     return df_input
+#
+# def strToDT():
+#     df = createTestSample()
+#     #df['basic|uitvoer'] = pd.to_datetime(df['basic|uitvoer'], format='%d-%m-&Y %H:%M:%S.%f')
+#     return df
+#
+# def changeD(df_complete):
+#     print(df_complete.columns)
+#     df_filter = df_complete[(df_complete["basic|drp_act"] == 'D') | (df_complete["basic|drp_act"] == 'A') | (df_complete["basic|drp_act"] == 'V')]
+#     df_K_A = df_filter
+#     df_K_V = df_filter.copy()
+#     df_K_A["basic|drp_act"] = df_K_A["basic|drp_act"].replace('D','K_A')
+#     df_K_A["basic|drp_act"] = df_K_A["basic|drp_act"].replace('A','K_A')
+#     df_K_A["basic|drp_act"] = df_K_A["basic|drp_act"].replace('V','K_A')
+#
+#     df_K_V["basic|drp_act"] = df_K_V["basic|drp_act"].replace('D','K_V')
+#     df_K_V["basic|drp_act"] = df_K_V["basic|drp_act"].replace('A','K_V')
+#     df_K_V["basic|drp_act"] = df_K_V["basic|drp_act"].replace('V','K_V')
+#
+#     x1 = datetime.datetime(2019, 1, 3).date()
+#     # x2 = datetime.datetime(2019, 5, 10).date()
+#     treinserie = "500E"
+#     treinnr = "501"
+#
+#     ka = df_K_A[(df_K_A['date'] == x1) & (df_K_A['basic_treinnr_treinserie'] == treinserie)]
+#     kv = df_K_V[(df_K_V['date'] == x1) & (df_K_V['basic_treinnr_treinserie'] == treinserie)]
+#
+#     print("LEN!:", len(df_K_A), len(df_K_V))
+#     print(ka[(ka['basic|treinnr'] == treinnr)])
+#     print(kv[(kv['basic|treinnr'] == treinnr)])
+#
+#     df_res = df_complete[~(df_complete["basic|drp_act"] == 'D') & ~(df_complete["basic|drp_act"] == 'A') & ~(df_complete["basic|drp_act"] == 'V')]
+#     df_res = df_res.append(df_K_A, ignore_index=True)
+#     df_res = df_res.append(df_K_V, ignore_index=True)
+#
+#     df_res = df_res.sort_values(by=['date', 'basic_treinnr_treinserie', "basic|treinnr", "basic|plan"])
+#
+#
+#
+#     df_res = df_res.reset_index(drop=True)
+#
+#     return df_res
+#
+#
+# pd.set_option('display.max_columns', None)
+# print(changeD(createTestSample()))
+#
+# #df1 = changeD(createTestSample())
+# # print(df1)
+#
+# d = {'basic|uitvoer': ["1-3-2019", "1-3-2019", "1-3-2019","1-3-2019"],
+#          'delay': [0,1,2,1],
+#          'basic|drp_act': ["V", "D", "A","D"],
+#          'basic_treinnr_treinserie': ["500E","500E","500E","500E"],
+#          'basic|treinnr': ["501","501","501","501"]}
+# df = pd.DataFrame(data=d)
+# df['basic|uitvoer'] = pd.to_datetime(df['basic|uitvoer'], format='%m-%d-%Y')
+# print(df.dtypes)
+
+def getNumberOfInterventions():
+    d = {'prl_rijw_eerst_rijweg_van': ['UF', "PD", "AF", "GH"],
+         'prl_rijw_laatst_rijweg_van': ['UF', "PD", "AF", "GH"],
+         'prl_rijw_eerst_rijweg_naar': [np.NaN, "CD", "EF", "GH"],
+         'prl_rijw_laatst_rijweg_naar': [np.NaN, "CD", "EF", "GH"],
+         'prl_rijw_eerst_rijweg_dwangnr': [-1, -1, -1, -1],
+         'prl_rijw_laatst_rijweg_dwangnr': [-1, -1, -1, -1] }
+
     df = pd.DataFrame(data=d)
+    #df = keepTrainseries(df, list_of_trainseries)
+    # vergelijk prl_rijw_eerst_rijweg_van, prl_rijw_eerst_rijweg_naar, prl_rijw_eerst_rijweg_dwangnr, MET prl_rijw_laatst_rijweg_van, prl_rijw_laatst_rijweg_naar prl_rijw_laatst_rijweg_dwangnr
+    df = df.fillna("Emp")
+    df['intervened'] = np.where( (df['prl_rijw_eerst_rijweg_van'] == df['prl_rijw_laatst_rijweg_van']) &
+                                 (df['prl_rijw_eerst_rijweg_naar'] == df['prl_rijw_laatst_rijweg_naar']) &
+                                 (df['prl_rijw_eerst_rijweg_dwangnr'] == df['prl_rijw_laatst_rijweg_dwangnr']), False, True )
+    pd.set_option('display.max_columns', None)
+    print(df.loc[(df['intervened'] == False)])
+    return (df['intervened'] == False).sum(), (df['intervened'] == True).sum()
 
-    df['basic|uitvoer'] = pd.to_datetime(df['basic|uitvoer'], format='%m-%d-%Y %H:%M:%S')
-    df['basic|plan'] = df['basic|uitvoer']
-    df['date'] = pd.to_datetime(df['basic|uitvoer']).dt.date
-    return df
-
-def keepActivity(df_input, act_val : List[str]):
-    df_input = df_input.loc[df_input['basic|drp_act'].isin(act_val)]
-    return df_input
-
-def strToDT():
-    df = createTestSample()
-    #df['basic|uitvoer'] = pd.to_datetime(df['basic|uitvoer'], format='%d-%m-&Y %H:%M:%S.%f')
-    return df
-
-def changeD(df_complete):
-    print(df_complete.columns)
-    df_filter = df_complete[(df_complete["basic|drp_act"] == 'D') | (df_complete["basic|drp_act"] == 'A') | (df_complete["basic|drp_act"] == 'V')]
-    df_K_A = df_filter
-    df_K_V = df_filter.copy()
-    df_K_A["basic|drp_act"] = df_K_A["basic|drp_act"].replace('D','K_A')
-    df_K_A["basic|drp_act"] = df_K_A["basic|drp_act"].replace('A','K_A')
-    df_K_A["basic|drp_act"] = df_K_A["basic|drp_act"].replace('V','K_A')
-
-    df_K_V["basic|drp_act"] = df_K_V["basic|drp_act"].replace('D','K_V')
-    df_K_V["basic|drp_act"] = df_K_V["basic|drp_act"].replace('A','K_V')
-    df_K_V["basic|drp_act"] = df_K_V["basic|drp_act"].replace('V','K_V')
-
-    x1 = datetime.datetime(2019, 1, 3).date()
-    # x2 = datetime.datetime(2019, 5, 10).date()
-    treinserie = "500E"
-    treinnr = "501"
-
-    ka = df_K_A[(df_K_A['date'] == x1) & (df_K_A['basic_treinnr_treinserie'] == treinserie)]
-    kv = df_K_V[(df_K_V['date'] == x1) & (df_K_V['basic_treinnr_treinserie'] == treinserie)]
-
-    print("LEN!:", len(df_K_A), len(df_K_V))
-    print(ka[(ka['basic|treinnr'] == treinnr)])
-    print(kv[(kv['basic|treinnr'] == treinnr)])
-
-    df_res = df_complete[~(df_complete["basic|drp_act"] == 'D') & ~(df_complete["basic|drp_act"] == 'A') & ~(df_complete["basic|drp_act"] == 'V')]
-    df_res = df_res.append(df_K_A, ignore_index=True)
-    df_res = df_res.append(df_K_V, ignore_index=True)
-
-    df_res = df_res.sort_values(by=['date', 'basic_treinnr_treinserie', "basic|treinnr", "basic|plan"])
-
-
-
-    df_res = df_res.reset_index(drop=True)
-
-    return df_res
-
-
-pd.set_option('display.max_columns', None)
-print(changeD(createTestSample()))
-
-#df1 = changeD(createTestSample())
-# print(df1)
-
-d = {'basic|uitvoer': ["1-3-2019", "1-3-2019", "1-3-2019","1-3-2019"],
-         'delay': [0,1,2,1],
-         'basic|drp_act': ["V", "D", "A","D"],
-         'basic_treinnr_treinserie': ["500E","500E","500E","500E"],
-         'basic|treinnr': ["501","501","501","501"]}
-df = pd.DataFrame(data=d)
-df['basic|uitvoer'] = pd.to_datetime(df['basic|uitvoer'], format='%m-%d-%Y')
-print(df.dtypes)
+print(getNumberOfInterventions())
--- Tests/infoDataSet.py
+++ Tests/infoDataSet.py
@@ -1,5 +1,7 @@
 from ETL_data import retrieveDataframe
 from typing import List
+import numpy as np
+import pandas as pd
 
 def getAllGroupLengths(grouped_by_date):
     total_lengths = [len(x) for x in grouped_by_date]
@@ -15,6 +17,25 @@ def keepTrainseries(df_input, act_val : List[str]):
     df_input = df_input.loc[df_input['basic_treinnr_treinserie'].isin(act_val)]
     return df_input
 
+def getNumberOfInterventions(export_name, list_of_trainseries):
+    df = pd.read_csv(export_name, sep=";")
+    df = keepTrainseries(df, list_of_trainseries)
+    # vergelijk prl_rijw_eerst_rijweg_van, prl_rijw_eerst_rijweg_naar, prl_rijw_eerst_rijweg_dwangnr, MET prl_rijw_laatst_rijweg_van, prl_rijw_laatst_rijweg_naar prl_rijw_laatst_rijweg_dwangnr
+
+    df['prl_rijw_eerst_rijweg_dwangnr'] = df['prl_rijw_eerst_rijweg_dwangnr'].apply(pd.to_numeric, errors='coerce')
+    df['prl_rijw_laatst_rijweg_dwangnr'] = df['prl_rijw_laatst_rijweg_dwangnr'].apply(pd.to_numeric, errors='coerce')
+
+    df[['prl_rijw_eerst_rijweg_van', 'prl_rijw_laatst_rijweg_van', 'prl_rijw_eerst_rijweg_naar', 'prl_rijw_laatst_rijweg_naar']] = df[['prl_rijw_eerst_rijweg_van', 'prl_rijw_laatst_rijweg_van', 'prl_rijw_eerst_rijweg_naar', 'prl_rijw_laatst_rijweg_naar']].fillna("Emp")
+    df[['prl_rijw_eerst_rijweg_dwangnr', 'prl_rijw_laatst_rijweg_dwangnr']] = df[['prl_rijw_eerst_rijweg_dwangnr', 'prl_rijw_laatst_rijweg_dwangnr']].fillna(0)
+
+    df['intervened'] = np.where( (df['prl_rijw_eerst_rijweg_van'] == df['prl_rijw_laatst_rijweg_van']) &
+                                 (df['prl_rijw_eerst_rijweg_naar'] == df['prl_rijw_laatst_rijweg_naar']) &
+                                 (df['prl_rijw_eerst_rijweg_dwangnr'] == df['prl_rijw_laatst_rijweg_dwangnr']), False, True )
+    pd.set_option('display.max_columns', None)
+    intv = (df['intervened'] == True).sum()
+    no_intv = (df['intervened'] == False).sum()
+    return ((100 /(no_intv + intv)) * intv).round(2)
+
 def getInfo(export_name):
     list_of_trainseries = ["500E","500O","600E","600O","700E","700O",'1800E',"1800O","6200E","6200O","8100E",'8100O']
     df = retrieveDataframe(export_name, None)
@@ -26,5 +47,6 @@ def getInfo(export_name):
     total_lengths = [len(x) for x in grouped_by_date]
     most_common_length = max(set(total_lengths), key=total_lengths.count)
     print("Largest group with length:", most_common_length)
+    print("Percentage of interventions", getNumberOfInterventions(export_name, list_of_trainseries))
 
 getInfo('../Data/2019-03-01_2019-05-31.csv')
