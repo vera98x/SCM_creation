779944cc66e72b73328e3409ea3d839684d5379c
vera98x <v.schoonderwoerd@outlook.com>
2023-02-17 14:27:55 +0100

    first hyperparameter nn for gamma

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
+import keras_tuner as kt
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
@@ -47,42 +48,85 @@ print(gamma_loss(100, 2).numpy())
 count, bins, ignored = plt.hist(y, 50, density=True)
 #plt.show()
 
-model = tf.keras.Sequential(
-    [
-        tf.keras.layers.Dense(
-            20, input_dim=1, activation="sigmoid", kernel_initializer="random_uniform"
-        ),
-        tf.keras.layers.Dense(
-            2, activation="relu", kernel_initializer="random_uniform"
-        ),
-        tfp.layers.DistributionLambda(
-            lambda t: tfd.Gamma(
-                concentration=tf.math.softplus(t[:, 0]) + 1e-9,
-                rate=tf.math.softplus(t[:, 1]) + 1e-9,
-            ),
-        ),
-    ]
-)
+# model = tf.keras.Sequential(
+#     [
+#         tf.keras.layers.Dense(
+#             20, input_dim=1, activation="sigmoid", kernel_initializer="random_uniform"
+#         ),
+#         tf.keras.layers.Dense(
+#             2, imput_dim = 20, activation="relu", kernel_initializer="random_uniform"
+#         ),
+#         tfp.layers.DistributionLambda(
+#             lambda t: tfd.Gamma(
+#                 concentration=tf.math.softplus(t[:, 0]) + 1e-9,
+#                 rate=tf.math.softplus(t[:, 1]) + 1e-9,
+#             ),
+#         ),
+#     ]
+# )
+
+def model_builder(hp):
+    hp_concentration = hp.Choice("hp_concentration", values=[1, 2,3,4])
+    hp_rate = hp.Choice("hp_rate", values=[1, 2,3,4])
+
+    model = tf.keras.Sequential()
+    model.add(tfp.layers.DistributionLambda(
+                    lambda t: tfd.Gamma(
+                        concentration=hp_concentration,
+                        rate=hp_rate,
+                    ),
+                ) )
+    negloglik = lambda y, p_y: -p_y.log_prob(y)
+    model.compile(loss=negloglik, optimizer=tf.optimizers.Adamax(learning_rate=1e-4))
+    return model
 
 
-negloglik = lambda y, p_y: -p_y.log_prob(y)
+tuner = kt.Hyperband(model_builder,
+                     objective='val_loss',
+                     max_epochs=10,
+                     factor=3,
+                     directory=None,
+                     project_name=None)
 
-model.compile(loss=negloglik, optimizer=tf.optimizers.Adamax(learning_rate=1e-4))
-model.fit(x, y, epochs=500, verbose=False)
+print(tuner.search_space_summary())
 
-y_model = model(x)
-y_sample = y_model.sample()
+stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+tuner.search(x, y, epochs=50, validation_split=0.2, callbacks=[stop_early])
+best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+print(best_hps)
+print(f"""
+The hyperparameter search is complete. The optimal number of units in the first densely-connected
+layer is ...., and the optimal learning rate for the optimizer
+is {best_hps.get('hp_concentration')}, {best_hps.get('hp_rate')}.
+""")
+
+# Build the model with the optimal hyperparameters and train it on the data for 50 epochs
+model = tuner.hypermodel.build(best_hps)
+history = model.fit(x, y, epochs=50, validation_split=0.2)
+
+val_acc_per_epoch = history.history['val_loss']
+best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+print('Best epoch: %d' % (best_epoch,))
+
+hypermodel = tuner.hypermodel.build(best_hps)
+
+# Retrain the model
+hypermodel.fit(x, y, epochs=best_epoch, validation_split=0.2)
+
+y_model = hypermodel(x)
+y_sample = y_model.sample(1000)
 y_hat = y_model.mean()
 y_sd = y_model.stddev()
 y_hat_m2sd = y_hat -2 * y_sd
 y_hat_p2sd = y_hat + 2*y_sd
 
-print("True μ: ", 1)
+print("True μ: ", y.numpy().mean())
 print("Estimated μ: ", y_hat.numpy().mean())
-print("True σ: ", 3/8)
+print("True σ: ",  np.std(y.numpy()))
 print("Estimated σ: ", y_sd.numpy().mean())
 
-plt.hist(y_model, 50, density=True)
+plt.hist(y, 50, density=True)
 plt.hist(y_sample, 50, density=True)
 
 fig, (ax1, ax2) =plt.subplots(1, 2, figsize=(15, 5), sharey=True)
@@ -90,8 +134,8 @@ ax1.scatter(x, y, alpha=0.4, label='data')
 ax1.scatter(x, y_sample, alpha=0.4, color='red', label='model sample')
 ax1.legend()
 ax2.scatter(x, y, alpha=0.4, label='data')
-ax2.plot(x, y_hat, color='red', alpha=0.8, label='model $\mu$')
-ax2.plot(x, y_hat_m2sd, color='green', alpha=0.8, label='model $\mu \pm 2 \sigma$')
-ax2.plot(x, y_hat_p2sd, color='green', alpha=0.8)
+ax2.plot(x, [y_hat]*1000, color='red', alpha=0.8, label='model $\mu$')
+ax2.plot(x, [y_hat_m2sd]*1000, color='green', alpha=0.8, label='model $\mu \pm 2 \sigma$')
+ax2.plot(x, [y_hat_p2sd]*1000, color='green', alpha=0.8)
 ax2.legend()
 plt.show()
